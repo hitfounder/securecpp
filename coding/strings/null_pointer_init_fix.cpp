ecedfc30d99a11cddb29f324290fb80f246aa78a
hitfounder <gmc@mail.ru>
2023-10-11 21:05:15 +0300

    Add gsl not null

--- coding/strings/null_pointer_init_fix.cpp
+++ coding/strings/null_pointer_init_fix.cpp
@@ -1,12 +1,16 @@
 #include <cstdlib>
+#include <gsl/pointers>
 #include <iostream>
 #include <string>
-  
+
 int main() {
     const char* const tmpEnv{std::getenv("TMP")};
     const std::string tmp{tmpEnv ? tmpEnv : ""};
     if (!tmp.empty()) {
         std::cout << tmp;
     }
+
+    const std::string path{gsl::make_not_null(std::getenv("TMP"))};
+    std::cout << path;
     return 0;
 }
