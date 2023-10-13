1662447ee05d467c36ded491224e8bc94efa5142
Sergey Talantov <sergey.talantov@kaspersky.com>
2023-10-13 15:29:50 +0300

    Add string_view null termination example

---
+++ coding/strings/string_view_null_termination.cpp
@@ -0,0 +1,18 @@
+#include <cstring>
+#include <iostream>
+#include <string_view>
+#include <string>
+
+void foo(std::string_view str) {
+    std::cout << "string view actual length: " << str.length() << std::endl;
+    std::cout << "strlen with string view: " << std::strlen(str.data()) << std::endl;
+    std::cout << "strlen with string: " << std::strlen(std::string(str).c_str()) << std::endl;
+}
+
+int main() {
+    std::string_view str{"Hello World"};
+    foo(str);
+    str.remove_suffix(5);
+    foo(str);
+    return 0;
+}
