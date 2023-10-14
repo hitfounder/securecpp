43c4c4396380a87a8c8118fd1a74795b0a01ea66
hitfounder <gmc@mail.ru>
2023-10-14 14:20:06 +0300

    Fix

--- coding/strings/sso_stack_overflow.cpp
+++ coding/strings/sso_stack_overflow.cpp
@@ -4,19 +4,18 @@
 #include <algorithm>
 
 int main() {
-    // String is allocated in stack
+    // Only 14 symbols, so string is allocated on stack
     std::string str{"11111111111111"};
 
-    // Save iterator pointed to stack
+    // Save iterator pointed to begin (contains stack address)
     auto begin{str.begin()};
-    // String is reallocated to heap
+
+    // Add more symbols, so string is reallocated to heap
     str.append("22222222222");
 
-    // Input data to dangling pointer causes stack overflow
+    // Input data is copied to dangling pointer, this causes stack overflow
     std::copy(std::istream_iterator<char>(std::cin),
               std::istream_iterator<char>(),
               std::inserter(str, begin));
-
-    std::cout << str;
     return 0;
 }
