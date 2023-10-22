b915a8b235d05c0b51015404a4c936ba256a3022
hitfounder <gmc@mail.ru>
2023-10-22 13:08:27 +0300

    Double free

---
+++ coding/memory/double_delete.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+
+class A {
+public:
+    A() : val(new int) {}
+    ~A() {
+        delete val;
+    }
+private:
+    int* val{nullptr};
+};
+
+class AFixed {
+public:
+    AFixed() : val(new int) {}
+    AFixed(const AFixed& other) {
+        val = new int(*other.val);
+    }
+    AFixed& operator=(AFixed& other) {
+        AFixed temp(other);
+        std::swap(val, temp.val);
+        return *this;
+    }
+    AFixed(AFixed&& other) {
+        val = other.val;
+        other.val = nullptr;
+    }
+    AFixed& operator=(AFixed&& other) {
+        val = other.val;
+        other.val = nullptr;
+        return *this;
+    }
+    ~AFixed() {
+        delete val;
+    }
+private:
+    int* val{nullptr};
+};
+
+int main() {
+    A a1, a2;
+    a2 = a1;
+
+    AFixed af1, af2;
+    af2 = af1;
+    return 0;
+}
