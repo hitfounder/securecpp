24cb166cb43bbd52b3416b579626ac05148e449d
Sergey Talantov <sergey.talantov@kaspersky.com>
2023-11-17 09:12:42 +0300

    Cmake fixed. Iterators updated.

--- coding/strings/iterator_invalidated_erase_fix.cpp
+++ coding/strings/iterator_invalidated_erase_fix.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <string>
 
-int main() {
+void fix() {
     std::string str{"hello"};
     str.erase(std::remove(str.begin(), str.end(), 'l'), str.end());
     std::cout << str << std::endl;
+}
+
+void fixCpp20() {
+    std::string str{"hello"};
+    std::erase(str, 'l');
+    std::cout << str << std::endl;
+}
+
+int main() {
+    fix();
+    fixCpp20();
     return 0;
 }
