c3a913b0bf1442bedeef69770bbe45a9a52127c5
hitfounder <gmc@mail.ru>
2023-12-16 19:14:30 +0300

    Race condition

---
+++ coding/threading/stack_race_condition.cpp
@@ -0,0 +1,63 @@
+#include <stack>
+#include <thread>
+#include <vector>
+
+template <typename T>
+class ThreadSafeStack {
+public:
+    void push(const T& val) {
+        std::lock_guard lock(m_lock);
+        m_stack.push(val);
+    }
+
+    T top() const {
+        std::lock_guard lock(m_lock);
+        return m_stack.top();
+    }
+
+    void pop() {
+        std::lock_guard lock(m_lock);
+        m_stack.pop();
+    }
+
+    bool empty() {
+        std::lock_guard lock(m_lock);
+        return m_stack.empty();
+    }
+
+    void checkAndPop() {
+        std::lock_guard lock(m_lock);
+        if (!m_stack.empty())
+            m_stack.pop();
+    }
+
+private:
+    std::stack<T> m_stack;
+    mutable std::mutex m_lock;
+};
+
+int main() {
+    ThreadSafeStack<int> gStack;
+
+    auto test = [&gStack]() {
+        for (int i = 0; i < 10; ++i) {
+            gStack.push(i);
+        }
+        //while (!gStack.empty()) {
+        //    gStack.pop();
+        //}
+        gStack.checkAndPop();
+    };
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 1000; ++i) {
+        threads.emplace_back(std::thread(test));
+    }
+
+    std::for_each(std::begin(threads), std::end(threads),
+    [](std::thread& t){
+        t.join();
+    });
+
+    return 0;
+}
