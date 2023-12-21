0c9ab9cff3a99a6f2b9fc8a968cf4e457ed9d3b9
hitfounder <gmc@mail.ru>
2023-12-21 09:31:22 +0300

    Add llfio

--- coding/files/races.cpp
+++ coding/files/races.cpp
@@ -1,5 +1,6 @@
 #include <filesystem>
 #include <fstream>
+#include <llfio/llfio.hpp>
 
 void CheckAndCreate(const std::filesystem::path& p) {
     if (!std::filesystem::exists(p)) {
@@ -26,16 +27,56 @@ void CheckAndCreateNoRace(const std::filesystem::path& p) {
 }
 
 void CheckAndUse(const std::filesystem::path& p) {
-    if (!std::filesystem::is_symlink(p)) {
+    if (std::filesystem::is_regular_file(p)) {
         std::fstream f(p.string(), std::ios_base::in | std::ios_base::out);
+        std::string data;
+        f >> data;
+        std::cout << data << std::endl;
     }
 }
 
-int main() {
+void CheckAndUseNoRace(const std::filesystem::path& p) {
+    namespace llfio = LLFIO_V2_NAMESPACE;
+
+    llfio::file_handle fh = llfio::file(
+        {},        // path_handle to base directory
+        p.string() // path_view to path fragment relative to base directory
+                   // default mode is read only
+                   // default creation is open existing
+                   // default caching is all
+                   // default flags is none
+        ).value(); // If failed, throw a filesystem_error exception
+
+    llfio::stat_t fhstat;
+        fhstat.fill(
+            fh      // file handle from which to fill stat_t
+                    // default stat_t::want is all
+        ).value();
+    
+    if (fhstat.st_type == std::filesystem::file_type::regular) {
+        std::vector<llfio::byte> buffer(fh.maximum_extent().value());
+        // Synchronous scatter read from file
+        llfio::file_handle::size_type bytesread = llfio::read(
+            fh,                                 // handle to read from
+            0,                                  // offset
+            {{ buffer.data(), buffer.size() }}  // Single scatter buffer of the vector 
+                                                // default deadline is infinite
+        ).value();                              // If failed, throw a filesystem_error exception
+        buffer.resize(bytesread);
+        for (const auto& c: buffer)
+            std::cout << static_cast<char>(c);
+    }
+}
+
+int main(int argc, char** argv) {
     CheckAndCreate("/tmp/file");
     std::filesystem::remove("/tmp/file");
 
     CheckAndCreateNoRace("/tmp/file");
     std::filesystem::remove("/tmp/file");
+
+    const std::string fileToUse{argc > 1 ? argv[1] : ""};
+    CheckAndUse(fileToUse);
+    CheckAndUseNoRace(fileToUse);
     return 0;
 }
