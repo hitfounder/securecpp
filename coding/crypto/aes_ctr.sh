141a27cea12d57953504186bb4bd1ebfd2012500
hitfounder <gmc@mail.ru>
2024-01-13 13:45:22 +0300

    Add aes ctr

--- coding/crypto/aes_ctr.sh
+++ coding/crypto/aes_ctr.sh
@@ -5,8 +5,8 @@ iv=$(<data/iv.txt)
 echo "Key: $key"
 echo "IV: $iv"
 echo "Data to encrypt: `cat data/in`"
-openssl enc -aes-256-cbc -in data/in -K "$key" -iv "$iv" -out data/out -v
+openssl enc -aes-256-ctr -in data/in -K "$key" -iv "$iv" -out data/out -v
 echo "Encrypted: `xxd -ps -u data/out`"
 
-openssl enc -d -aes-256-cbc -in data/out -K "$key" -iv "$iv" -out data/dec -v
+openssl enc -d -aes-256-ctr -in data/out -K "$key" -iv "$iv" -out data/dec -v
 echo "Decrypted: `cat data/dec`"
--- coding/crypto/aes_ecb.sh
+++ coding/crypto/aes_ecb.sh
@@ -6,5 +6,5 @@ echo "Data to encrypt: `cat data/in`"
 openssl enc -aes-256-ecb -in data/in -K "$key" -out data/out -v
 echo "Encrypted: `xxd -ps -u data/out`"
 
-openssl enc -d -aes-256-ecb -in data/out -K "$(<data/key.txt)" -out data/dec -v
+openssl enc -d -aes-256-ecb -in data/out -K "$key" -out data/dec -v
 echo "Decrypted: `cat data/dec`"
