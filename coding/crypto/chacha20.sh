272693df8681b66a3036ff8b3034bf4d06b54121
hitfounder <gmc@mail.ru>
2024-01-13 15:02:52 +0300

    Add nopad option to chacha20

--- coding/crypto/chacha20.sh
+++ coding/crypto/chacha20.sh
@@ -5,8 +5,10 @@ iv=$(<data/iv.txt)
 echo "Key: $key"
 echo "IV: $iv"
 echo "Data to encrypt: `cat data/in`"
-openssl enc -chacha20 -in data/in -K "$key" -iv "$iv" -out data/out -v
+# Here could be -nopad option, because padding as absent in stream cypher
+# For block cypher, like aes, -nopad option will return error if input data is not multiple block size
+openssl enc -chacha20 -in data/in -K "$key" -iv "$iv" -out data/out -v -nopad
 echo "Encrypted: `xxd -ps -u data/out`"
 
-openssl enc -d -chacha20 -in data/out -K "$key" -iv "$iv" -out data/dec -v
+openssl enc -d -chacha20 -in data/out -K "$key" -iv "$iv" -out data/dec -v -nopad
 echo "Decrypted: `cat data/dec`"
