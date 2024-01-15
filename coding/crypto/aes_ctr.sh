f8c2c48c728e625d088021e96af99567f354ebbf
hitfounder <gmc@mail.ru>
2024-01-15 19:04:37 +0300

    Updated

--- coding/crypto/aes_ctr.sh
+++ coding/crypto/aes_ctr.sh
@@ -5,8 +5,10 @@ iv=$(<data/iv128.txt)
 echo "Key: $key"
 echo "IV: $iv"
 echo "Data to encrypt: `cat data/in`"
-openssl enc -aes-256-ctr -in data/in -K "$key" -iv "$iv" -out data/out -v
+# AES in CTR mode is a kind of stream cipher,
+# so it does not require padding, you could specify -nopad
+openssl enc -aes-256-ctr -in data/in -K "$key" -iv "$iv" -out data/out -v -nopad
 echo "Encrypted: `xxd -ps -u data/out`"
 
-openssl enc -d -aes-256-ctr -in data/out -K "$key" -iv "$iv" -out data/dec -v
+openssl enc -d -aes-256-ctr -in data/out -K "$key" -iv "$iv" -out data/dec -v -nopad
 echo "Decrypted: `cat data/dec`"
