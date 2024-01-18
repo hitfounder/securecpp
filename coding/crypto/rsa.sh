e2cd0a5d3c9294f1688c5649e025c07d482f9474
hitfounder <gmc@mail.ru>
2024-01-18 08:38:32 +0300

    Add rsa

---
+++ coding/crypto/rsa.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+privKey=data/rsa_priv.pem
+pubKey=data/rsa_pub.pem
+
+inputData=data/in
+encryptedData=data/out
+decrtptedData=data/dec
+
+# Generate private key
+openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:65537 -out $privKey -outform PEM -quiet
+echo "Generated private key:"
+(head -n 4; echo "..."; tail -n 4) < $privKey
+
+echo
+
+# Get public key from private
+openssl rsa -in $privKey -pubout -out $pubKey
+echo "Generated public key:"
+(head -n 4; echo "..."; tail -n 4) < $pubKey
+
+echo
+
+# Encrypting data using public key
+openssl pkeyutl -encrypt -in $inputData -inkey $pubKey -pubin -out $encryptedData
+echo "Encrypted: `xxd -ps -u $encryptedData`"
+
+# Decrypting data using private key
+openssl pkeyutl -decrypt -in $encryptedData -inkey $privKey -out $decrtptedData
+echo "Decrypted: `cat $decrtptedData`"
