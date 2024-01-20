f0df42bb90a8933b3811d7fa089fffc39a66a8a7
hitfounder <gmc@mail.ru>
2024-01-20 13:40:59 +0300

    DH optimized

--- coding/crypto/dh.sh
+++ coding/crypto/dh.sh
@@ -11,39 +11,40 @@ pubKey2=data/dh_pub_2.pem
 sharedSecret1=data/secret1
 sharedSecret2=data/secret2
 
+generate_private_key() {
+    openssl genpkey -paramfile $1 -out $2
+    echo "Generated private key:"
+    (head -n 4; echo "..."; tail -n 4) < $2
+}
+
+generate_public_key() {
+    openssl pkey -in $1 -pubout -out $2
+    echo "Generated public key:"
+    (head -n 4; echo "..."; tail -n 4) < $2
+}
+
 # Create DH parameters
 openssl genpkey -genparam -algorithm DH -out $param -pkeyopt dh_paramgen_prime_len:2048 -pkeyopt dh_paramgen_generator:2
 
 echo "DH parameters:"
 openssl pkeyparam -in $param -text
-
 echo
 
 # Generate first private key
-openssl genpkey -paramfile $param -out $privKey1
-echo "Generated private key 1:"
-(head -n 4; echo "..."; tail -n 4) < $privKey1
-
+generate_private_key $param $privKey1
 echo
 
 # Generate second private key
-openssl genpkey -paramfile $param -out $privKey2
-echo "Generated private key 2:"
-(head -n 4; echo "..."; tail -n 4) < $privKey2
-
+generate_private_key $param $privKey2
 echo
 
 # Get first public key from private
-openssl pkey -in $privKey1 -pubout -out $pubKey1
-echo "Generated public key 1:"
-(head -n 4; echo "..."; tail -n 4) < $pubKey1
-
+generate_public_key $privKey1 $pubKey1
 echo
 
 # Get second public key from private
-openssl pkey -in $privKey2 -pubout -out $pubKey2
-echo "Generated public key 2:"
-(head -n 4; echo "..."; tail -n 4) < $pubKey2
+generate_public_key $privKey2 $pubKey2
+echo
 
 # Get shared secret for user 1
 openssl pkeyutl -derive -inkey $privKey1 -peerkey $pubKey2 -out $sharedSecret1
@@ -51,5 +52,5 @@ openssl pkeyutl -derive -inkey $privKey1 -peerkey $pubKey2 -out $sharedSecret1
 # Get shared secret for user 2
 openssl pkeyutl -derive -inkey $privKey2 -peerkey $pubKey1 -out $sharedSecret2
 
-# Secret will be the same
+# Secrets will be the same
 cmp $sharedSecret1 $sharedSecret2
