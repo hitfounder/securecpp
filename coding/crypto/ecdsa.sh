240ec374805581a5ed8058623b68d0c662b6507f
Sergey Talantov <sergey.talantov@kaspersky.com>
2024-01-23 14:52:31 +0300

    Add ecdsa

---
+++ coding/crypto/ecdsa.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+privKey=data/ecdsa_pss_priv.pem
+pubKey=data/ecdsa_pss_pub.pem
+
+inputData=data/in
+hash=data/hash
+signature=data/signature
+
+# Generate private key
+openssl genpkey -algorithm EC \
+    -pkeyopt ec_paramgen_curve:P-256 \
+    -pkeyopt ec_param_enc:named_curve \
+    -out $privKey -outform PEM
+echo "Generated private key:"
+(head -n 4; echo "..."; tail -n 4) < $privKey
+openssl pkey -text -in $privKey
+
+# Get public key from private
+openssl pkey -in $privKey -pubout -out $pubKey
+echo "Generated public key:"
+(head -n 4; echo "..."; tail -n 4) < $pubKey
+openssl pkey -text -pubin -in $pubKey
+
+# Calculate hash
+openssl dgst -sha256 -binary -out $hash $inputData
+echo "Hash of input file is: `xxd -ps -u $hash`"
+
+# Sign
+openssl pkeyutl -sign \
+  -in $hash -inkey $privKey \
+  -out $signature \
+  -pkeyopt digest:sha256
+echo "Signature is: `xxd -ps -u $signature`"
+
+# Verify
+openssl pkeyutl -verify \
+  -in $hash -sigfile $signature \
+  -pubin -inkey $pubKey \
+  -pkeyopt digest:sha256
