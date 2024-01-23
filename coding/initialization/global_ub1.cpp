480b34b7fa07061516ca67c067694c2699b08bf9
Sergey Talantov <sergey.talantov@kaspersky.com>
2024-01-23 14:05:19 +0300

    Build fix

--- coding/initialization/global_ub1.cpp
+++ coding/initialization/global_ub1.cpp
@@ -10,5 +10,10 @@ constexpr int StaticCompute(int n)
 {
     return n * n;
 }
+
+#if __cpp_constinit >= 201907L
 // "constexpr" could make this variable bound to current tranlation unit
 constinit int safeA = StaticCompute(10);
+#else
+int safeA = 100;
+#endif
