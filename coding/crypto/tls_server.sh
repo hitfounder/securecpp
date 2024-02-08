1d8936359a0487645a813657dda30eac3ca43b98
hitfounder <gmc@mail.ru>
2024-02-08 08:54:00 +0300

    Updated

--- coding/crypto/tls_server.sh
+++ coding/crypto/tls_server.sh
@@ -4,5 +4,5 @@ cert=data/tls_cert.pem
 key=data/tls_key.pem
 port=11111
 
-openssl req -x509 -nodes -days 1000 -newkey rsa:2048 -keyout $key -out $cert -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
-openssl s_server -accept $port -cert $cert -key $key
+openssl req -x509 -noenc -days 1000 -newkey rsa:2048 -keyout $key -out $cert -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
+openssl s_server -tls1_3 -accept $port -cert $cert -key $key
