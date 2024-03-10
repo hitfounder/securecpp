9833d7e3d68db2f0956f7b5b9bbc6d220e942270
hitfounder <gmc@mail.ru>
2024-03-10 17:43:16 +0300

    Single use updated

--- patterns/single_use_object/main.cpp
+++ patterns/single_use_object/main.cpp
@@ -1,45 +1,51 @@
 #include <iostream>
 #include <optional>
 #include <string>
-#include <string.h>
 
-#define __STDC_WANT_LIB_EXT1__ 1
+using SecureData = std::string;
 
-template <typename T>
 class SingleUse {
 public:
-    explicit SingleUse(const T& data)
-        : m_data(data) {}
+    explicit SingleUse(const SecureData& secureData)
+        : m_secureData(secureData) {}
 
-    T get() {
-        if (m_data.has_value()) {
-            std::optional<T> data;
-            std::swap(m_data, data);
-            return data.value();
+    SingleUse(const SingleUse&) = delete;
+    SingleUse& operator=(const SingleUse&) = delete;
+
+    SingleUse(SingleUse&&) = default;
+    SingleUse& operator=(SingleUse&&) = default;
+
+    SecureData extract() {
+        if (m_secureData.has_value()) {
+            OptionalSecureData secureData;
+            std::swap(m_secureData, secureData);
+            return secureData.value();
         }
         throw std::runtime_error("Value already used");
     }
     
 private:
-    mutable std::optional<T> m_data;
+    using OptionalSecureData = std::optional<SecureData>;
+
+    mutable OptionalSecureData m_secureData;
 };
 
-std::string RequestPassword() {
+SecureData RequestPassword() {
     // Password is requested here
     return "password";
 }
 
-void Authenticate(const std::string& password) {
+void Authenticate(const SecureData& password) {
     // Authentication is performed here
     std::cout << "Authentication with password: " << password << std::endl;
 }
 
 int main(int, char**){
     SingleUse password(RequestPassword());    
-    Authenticate(password.get());
+    Authenticate(password.extract());
 
     // Here will be exception
-    password.get();
+    password.extract();
 
     return 0;
 }
