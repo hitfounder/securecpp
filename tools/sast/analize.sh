d78a1c8a8b248ef8316f680085fc347ca3be7ede
hitfounder <gmc@mail.ru>
2024-10-14 20:32:56 +0300

    Add static analizers commands

---
+++ tools/sast/analize.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+rm -rf CMakeFiles/ CMakeCache.txt pvs_report clang_report
+scan-build cmake .
+scan-build -o clang_report make
+
+pvs-studio-analyzer trace -- make
+pvs-studio-analyzer analyze
+plog-converter -a GA:1,2 -t fullhtml PVS-Studio.log -o pvs_report
