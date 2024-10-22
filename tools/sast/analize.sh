d284ccd0895698aed7e89d074088d3be7241e455
Sergey Talantov <sergey.talantov@kaspersky.com>
2024-10-22 13:17:46 +0300

    Get rid of PVS free usage
    Change-Id: I42d3b384efdc0e60eac0105e87e52d7bdd449b60

--- tools/sast/analize.sh
+++ tools/sast/analize.sh
@@ -4,7 +4,6 @@ rm -rf CMakeFiles/ CMakeCache.txt pvs_report clang_report
 scan-build cmake .
 scan-build -o clang_report make
 
-pvs-studio-analyzer credentials PVS-Studio Free FREE-FREE-FREE-FREE
 pvs-studio-analyzer trace -- make
 pvs-studio-analyzer analyze
 plog-converter -a GA:1,2 -t fullhtml PVS-Studio.log -o pvs_report
