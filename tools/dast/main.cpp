094fc8e25af1f99f80f8d828cba98d679088e33a
Sergey Talantov <sergey.talantov@kaspersky.com>
2024-10-22 15:34:29 +0300

    Add dast example
    Change-Id: Ie1fd955bfd3e914566e4d220021bd5e85fea4801

---
+++ tools/dast/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    const bool heapOverflow{argc > 1 && argv[1] == std::string("heap_overflow")};
+
+    // Small strings will be allocated at stack because of SSO.
+    // Large strings are allocated at heap.
+    // So we could select heap or stack overflow.
+    std::string str(heapOverflow ? 100 : 5, 'H');
+    const auto begin{str.begin()};
+    const auto end(str.end());
+
+    str.insert(end, 200, '+');
+    str.insert(begin, 200, '-');
+    std::cout << str << std::endl;
+    return 0;
+}
