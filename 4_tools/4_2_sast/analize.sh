867d5b4597292f7ad564edd24ad1b4380dd7c52b
hitfounder <gmc@mail.ru>
2024-11-25 18:47:58 +0300

    Fix PVS usage

--- 4_tools/4_2_sast/analize.sh
+++ 4_tools/4_2_sast/analize.sh
@@ -6,4 +6,4 @@ scan-build -o clang_report make
 
 pvs-studio-analyzer trace -- make
 pvs-studio-analyzer analyze
-plog-converter -a GA:1,2 -t fullhtml PVS-Studio.log -o pvs_report
+plog-converter -a GA:1,2,3 -t fullhtml PVS-Studio.log -o pvs_report
