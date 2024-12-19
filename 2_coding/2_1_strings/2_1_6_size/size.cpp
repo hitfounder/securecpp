6b22c8a771f00828fc05fc0078f332040f92a5bc
Sergey Talantov <sergey.talantov@kaspersky.com>
2024-12-19 20:04:59 +0300

    Add string size example
    Change-Id: I2c1acca910845ef1159be23888a6b723db6b90c0

---
+++ 2_coding/2_1_strings/2_1_6_size/size.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+#define COUNTOF_C(array) (sizeof(array) / sizeof(array[0]))
+
+template <typename T, size_t N>
+char (&ArraySizeHelper(T (&array)[N]))[N];
+#define COUNTOF_CPP(array) (sizeof(ArraySizeHelper(array)))
+
+template <typename T, size_t N>
+constexpr size_t countof(T (&)[N]) noexcept
+{
+ return N;
+}
+
+template <typename Cont>
+constexpr auto countof(const Cont &cont) noexcept(noexcept(cont.size()))
+    -> decltype(cont.size())
+{
+  return cont.size();
+}
+
+int main() {
+    {
+        char str[] {"Hello world"};
+        std::cout << "Length of C-string \"" << str << "\"" << std::endl;
+        std::cout << "COUNTOF_C: " << COUNTOF_C(str) << std::endl;
+        std::cout << "COUNTOF_CPP: " <<COUNTOF_CPP(str) << std::endl;
+        std::cout << "countof: "<< countof(str) << std::endl;
+    }
+
+    {
+        std::string str{"Hello world"};
+        std::cout << "Length of std::string \"" << str << "\"" << std::endl;
+        std::cout << "COUNTOF_C: " << COUNTOF_C(str) << std::endl;
+        // Compilation error
+        // std::cout << "COUNTOF_CPP: " << COUNTOF_CPP(str) << std::endl;
+        std::cout << "countof: " << countof(str) << std::endl;
+        std::cout << "size: " << str.size() << std::endl;
+        std::cout << "std::size: " << std::size(str) << std::endl;
+    }
+    return 0;
+}
